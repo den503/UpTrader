e667f6df29f1c42585c38b15407b697f2eb18959
den503 <zaychuk.dd@gmail.com>
2020-02-19 08:45:14 +0300

    update project and add requirements.txt

--- menu/views.py
+++ menu/views.py
@@ -6,13 +6,13 @@ from menu.models import Item
 
 def get_menu(request, slug=None):
     item = None
-    items = Item.objects.prefetch_related('child').all()
+    items = Item.objects.select_related('child')
     if slug:
         item = Item.objects.get(slug=slug)
 
     template_name = 'index.html'
     context = {
-        # 'item': item,
-        'items': items,
+        'item': item,
+        'items': items.exclude(parent__gte=0),
     }
     return render(request, template_name, context)
